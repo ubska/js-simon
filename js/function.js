6a2fb66001e62e0dadb554b2c4e0fd5a55f45c23
Oleksandra Ubska <alessandraubska@gmail.com>
2024-06-17 14:30:19 +0200

    add function numeri casuali

---
+++ js/function.js
@@ -0,0 +1,14 @@
+// funzione che genera numeri casuali (array)
+function generaNumeriCasuali(min, max, numero) {
+    const numeriCasuali = [];
+    while (numeriCasuali.length < numero) {
+        const random = Math.floor(Math.random() * (max - min + 1)) + min;
+        if (!numeriCasuali.includes(random)) {
+            numeriCasuali.push(random);
+        }
+    }
+    return numeriCasuali;
+}
+
+
+console.log(generaNumeriCasuali(1, 100, 5));
